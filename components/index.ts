5926c3f7a9feaaf6c88940241b81066567814f35
afzalimdad9 <afzalimdad9@gmail.com>
2024-10-20 18:45:41 +0500

    add layout providers for clerk and themes

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import { ThemeProvider } from "./providers/ThemeProvider";
+import ClerkClientProvider from "./providers/ClerkProvider";
+export {
+    ThemeProvider,
+    ClerkClientProvider,
+}
---
+++ config/site.ts
@@ -0,0 +1,6 @@
+interface SiteConfig { title: string; description: string };
+
+export const siteConfig: SiteConfig = {
+    title: "Briefly",
+    description: "Condense text effortlessly with Briefly.",
+}
